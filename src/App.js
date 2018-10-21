9a5836c67380fa0891e525a4d8b8c671c4a0a849
Micah Caffey <mrcaffey@gmail.com>
2018-10-21 12:54:00 -0600

    added few basic functions for adding notes

--- src/App.js
+++ src/App.js
@@ -2,6 +2,24 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      noteText: ' ',
+    }
+  }
+
+  updateNoteText(noteText) {
+    this.setState({ noteText: noteText.target.value })
+  }
+
+  handleKeyPress = (event) => {
+    if (event.key === 'Enter') {
+
+    }
+  }
+
   render() {
     return (
       <div className="container">
@@ -12,6 +30,10 @@ class App extends Component {
 
         <input type="text"
             ref={((input) => {this.textInput =  input })} 
+            className="textInput"
+            value={this.state.noteText}
+            onChangeText={noteText => this.updateNoteText(noteText)}
+            onKeyPress={this.handleKeyPress.bind(this)}
         />
       </div>
     );
