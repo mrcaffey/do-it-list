e8a161129b52e25cb342902cb3474a654e146146
Micah Caffey <mrcaffey@gmail.com>
2018-10-21 13:59:53 -0600

    added functionality to add notes to app

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Note from './components/Note';
 import './App.css';
 
 class App extends Component {
@@ -15,25 +16,50 @@ class App extends Component {
     this.setState({ noteText: noteText.target.value })
   }
 
+  addNote() {
+    if (this.state.noteText === '') {return}
+
+    let notesArr = this.state.notes;
+    notesArr.push(this.state.noteText);
+    this.setState({ noteText: ''});
+    this.textInput.focus();
+
+  }
+
   handleKeyPress = (event) => {
     if (event.key === 'Enter') {
 
     }
   }
 
+  deleteNote(index) {
+    let notesArr = this.state.notes;
+    notesArr.splice(index, 1)
+    this.setState({ notes: notesArr })
+  }
+
   render() {
+
+    let notes = this.state.notes.map((val, key) => {
+      return <Note key={key} text={val}
+              deleteMethod={ () => this.deleteNote(key) } />
+    })
+
+
+
     return (
       <div className="container">
 
         <div className="header">Do-It-List</div>
+        {notes}
 
-        <div className="btn">+</div>
+        <div className="btn" onClick={this.addNote.bind(this)}>+</div>
 
         <input type="text"
             ref={((input) => {this.textInput =  input })} 
             className="textInput"
             value={this.state.noteText}
-            onChangeText={noteText => this.updateNoteText(noteText)}
+            onChange={noteText => this.updateNoteText(noteText)}
             onKeyPress={this.handleKeyPress.bind(this)}
         />
       </div>
